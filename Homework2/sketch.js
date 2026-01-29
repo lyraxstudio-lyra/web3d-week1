1cc8793e646688ccd76f16fe98e8d65175a5c74f
lyraxstudio-lyra <lyraxstudio@gmail.com>
2026-01-29 16:32:00 -0500

    subfolder_w_updateonHW2

---
+++ Homework2/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+    let canvas = createCanvas(600,600,WEBGL);
+    angleMode(DEGREES);
+}
+
+function draw(){
+    background(0);
+    fill(200,100,100);
+    noStroke();
+    orbitControl();
+    box(100);
+    translate(0,0,100);
+    rotateX(30);
+    box(50);
+    translate(0,0,50);
+    rotateX(60);
+    box(25);
+    translate(0,0,25);
+    rotateX(30);
+    scale(0.5);
+    box(25);
+}
