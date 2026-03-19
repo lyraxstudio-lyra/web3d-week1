574b12e3a07a045f1ae7201c864b189e37063d93
lyraxstudio-lyra <lyraxstudio@gmail.com>
2026-03-19 15:54:42 -0400

    updAates

--- Scene/script.js
+++ Scene/script.js
@@ -1,13 +1,105 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
-import { OrbitControls } from './src/OrbitControls.js';
-import { PointerLockControls } from './src/PointerLockControls.js';
+// The plug-in for orbit controls
+import { OrbitControls } from "./src/OrbitControls.js";
 
+// The plug-in for First Person Controls
+import { PointerLockControls } from "./src/PointerLockControls.js";
 let camera, canvas, controls, scene, renderer;
+let raycaster;
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = true;
 
+let prevTime = performance.now();
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
+
+init();
 function init() {
     // scene setup
     canvas = document.getElementById("3-holder");
     scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0xbfeff5 );
+    scene.background = new THREE.Color(0xbfeff5);
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    //renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize(innerWidth, innerHeight);
+    renderer.setAnimationLoop(animate);
+    canvas.appendChild(renderer.domElement);
+
+
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.set(0, 10, 0);
+    
+        controls = new PointerLockControls( camera, document.body );
+
+    const blocker = document.getElementById( 'blocker' );
+    const instructions = document.getElementById( 'instructions' );
+
+    instructions.addEventListener( 'click', function () {
+
+        controls.lock();
+
+    } );
+     controls.addEventListener( 'lock', function () {
+
+        instructions.style.display = 'none';
+        blocker.style.display = 'none';
+
+    } );
+
+    controls.addEventListener( 'unlock', function () {
+
+        blocker.style.display = 'block';
+        instructions.style.display = '';
+
+    } );
+        scene.add( controls.object );
+
+
+}
+function animate() {
+    
+    // Start First Person Control Animations
+    const time = performance.now();
+    if ( controls.isLocked === true ) {
+        const delta = ( time - prevTime ) / 1000;
+
+        velocity.x -= velocity.x * 10.0 * delta;
+        velocity.z -= velocity.z * 10.0 * delta;
+
+        velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+        direction.z = Number( moveForward ) - Number( moveBackward );
+        direction.x = Number( moveRight ) - Number( moveLeft );
+        direction.normalize(); // this ensures consistent movements in all directions
+
+        if ( moveForward || moveBackward ) velocity.z -= direction.z * 400.0 * delta;
+        if ( moveLeft || moveRight ) velocity.x -= direction.x * 400.0 * delta;
+
+        controls.moveRight( - velocity.x * delta );
+        controls.moveForward( - velocity.z * delta );
+        
+        // jump fix
+        controls.object.position.y += ( velocity.y * delta );
+        if ( controls.object.position.y < 10 ) {
+            velocity.y = 0;
+            controls.object.position.y = 10;
+
+            canJump = true;
+        }
+    }
+
+    prevTime = time;
+    // End First Person Control Animations
+    
+    render();
+}
+
+
+
+function render() {
+    renderer.render( scene, camera );
 }
