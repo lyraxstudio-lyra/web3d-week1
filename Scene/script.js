cb2ab8315c97dc7985eff8e1ac677dd37a4391b6
lyraxstudio-lyra <lyraxstudio@gmail.com>
2026-03-22 17:20:15 -0400

    sceneFinal

--- Scene/script.js
+++ Scene/script.js
@@ -7,6 +7,7 @@ import { PointerLockControls } from "./src/PointerLockControls.js";
 
 let camera, canvas, controls, scene, renderer;
 
+let raycaster;
 let moveForward = false;
 let moveBackward = false;
 let moveLeft = false;
@@ -55,6 +56,64 @@ async function init() {
 
     scene.add(controls.object);
 
+    const onKeyDown = function (event) {
+        switch (event.code) {
+            case "ArrowUp":
+            case "KeyW":
+                moveForward = true;
+                break;
+
+            case "ArrowLeft":
+            case "KeyA":
+                moveLeft = true;
+                break;
+
+            case "ArrowDown":
+            case "KeyS":
+                moveBackward = true;
+                break;
+
+            case "ArrowRight":
+            case "KeyD":
+                moveRight = true;
+                break;
+
+            case "Space":
+                if (canJump === true) velocity.y += 350;
+                canJump = false;
+                break;
+        }
+    };
+
+    const onKeyUp = function (event) {
+        switch (event.code) {
+            case "ArrowUp":
+            case "KeyW":
+                moveForward = false;
+                break;
+
+            case "ArrowLeft":
+            case "KeyA":
+                moveLeft = false;
+                break;
+
+            case "ArrowDown":
+            case "KeyS":
+                moveBackward = false;
+                break;
+
+            case "ArrowRight":
+            case "KeyD":
+                moveRight = false;
+                break;
+        }
+    };
+
+    document.addEventListener("keydown", onKeyDown);
+    document.addEventListener("keyup", onKeyUp);
+
+    raycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, -1, 0), 0, 10);
+
     // lights
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
     scene.add(ambientLight);
@@ -65,6 +124,8 @@ async function init() {
 
     // LOAD obj
     const OBJloader = new OBJLoader();
+
+    // obj1 boat
     const object1 = await OBJloader.loadAsync("./src/boat1.obj");
 
     object1.traverse((child) => {
@@ -78,21 +139,23 @@ async function init() {
 
     object1.scale.set(5, 5, 5);
     object1.position.set(0, 0, -80);
-    
+
+    // obj2 plate
     const object2 = await OBJloader.loadAsync("./src/plate.obj");
 
     object2.traverse((child) => {
         if (child.isMesh) {
             child.material = new THREE.MeshPhongMaterial({
-                color: 0x735839,
+                color: 0xc7f1ff,
                 flatShading: true
             });
         }
     });
-        object2.scale.set(10, 10,10);
+    object2.scale.set(10, 10, 10);
     object2.position.set(-25, 0, -30);
-    
-        const object3 = await OBJloader.loadAsync("./src/tree.obj");
+
+    // obj3 tree
+    const object3 = await OBJloader.loadAsync("./src/tree.obj");
 
     object3.traverse((child) => {
         if (child.isMesh) {
@@ -106,28 +169,40 @@ async function init() {
     object3.scale.set(5, 5, 5);
     object3.position.set(40, -10, -120);
 
+    // obj4 cloud1
+    const object4 = await OBJloader.loadAsync("./src/cloud.obj");
 
-    //add obj
+    object4.traverse((child) => {
+        if (child.isMesh) {
+            child.material = new THREE.MeshPhongMaterial({
+                color: 0xffc2b3,
+                flatShading: true
+            });
+        }
+    });
+    object4.scale.set(30, 30, 30);
+    object4.position.set(-205, 0, -130);
+
+    // obj5 cloud2
+    const object5 = await OBJloader.loadAsync("./src/cloud.obj");
+
+    object5.traverse((child) => {
+        if (child.isMesh) {
+            child.material = new THREE.MeshPhongMaterial({
+                color: 0xf0e1c2,
+                flatShading: true
+            });
+        }
+    });
+    object5.scale.set(60, 60, 60);
+    object5.position.set(-35, 10, -270);
+
+    // add obj
     scene.add(object1);
     scene.add(object2);
     scene.add(object3);
-    
-    //bg
-    //const textureLoader = new THREE.TextureLoader();
-    //const texture = await textureLoader.loadAsync("./src/bg2.jpeg");
-    //
-    //
-    //const geometry = new THREE.PlaneGeometry(300, 200);
-    //const material = new THREE.MeshBasicMaterial({
-    //    map: texture,
-    //    side: THREE.DoubleSide
-    //});
-    //
-    //const plane = new THREE.Mesh(geometry, material);
-    //
-    //plane.position.set(0, 50, -50);
-    //
-    //scene.add(plane);
+    scene.add(object4);
+    scene.add(object5);
 
     const textureLoader = new THREE.TextureLoader();
     const envTexture = await textureLoader.loadAsync("./src/bg2.jpg");
@@ -136,22 +211,21 @@ async function init() {
 
     scene.environment = envTexture;
     scene.background = envTexture;
-    
-const tex = await new THREE.TextureLoader().loadAsync("./src/waterSurface.jpeg");
 
-const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(400, 400),
-    new THREE.MeshStandardMaterial({
-        map: tex,
-        transparent: true,
-        opacity: 0.8   
-    })
-);
+    const tex = await new THREE.TextureLoader().loadAsync("./src/waterSurface.jpeg");
 
-plane.position.set(0, 0, 30);
-plane.rotation.x = -Math.PI / 2;
-scene.add(plane);
-    
+    const plane = new THREE.Mesh(
+        new THREE.PlaneGeometry(1000, 1000),
+        new THREE.MeshStandardMaterial({
+            map: tex,
+            transparent: true,
+            opacity: 0.8
+        })
+    );
+
+    plane.position.set(0, -25, 0);
+    plane.rotation.x = -Math.PI / 2;
+    scene.add(plane);
 }
 
 function animate() {
